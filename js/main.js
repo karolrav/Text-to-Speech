f00b976e6e6cbe116e29c250d496ff31799fc9d5
Karol <karolravdo@gmail.com>
2022-01-29 13:50:38 +0200

    add speak function, and gif

--- js/main.js
+++ js/main.js
@@ -30,3 +30,56 @@ getVoices();
 if(synth.onvoiceschanged !== undefined){
     synth.onvoiceschanged = getVoices;
 }
+
+const speak = () => {
+
+    if(synth.speaking) {
+        console.error("already speaking")
+    }
+    if(textInput.value !== '') {
+
+    body.style.background ="#141414 url('../img/wave.gif')";
+    body.style.backgroundRepeat = 'repeat-x'
+    body.style.backgroundSize = '100%  100%';
+
+const speakText = new SpeechSynthesisUtterance(textInput.value);
+    speakText.onend = e => {
+        body.style.background ="#141414"
+    }
+
+    speakText.onerror = e => {
+        console.log("erroras");
+    }
+    // voices
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+    voices.forEach(voice => {
+        if(voice.name === selectedVoice){
+            speakText.voice = voice;
+        }
+    });
+
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+    synth.speak(speakText);
+    
+}
+};
+
+textForm.addEventListener('submit', e => {
+    e.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+
+rate.addEventListener('change', e => 
+    rateValue.textContent = rate.value
+)
+
+pitch.addEventListener('change', e => 
+    pitchValue.textContent = pitch.value
+)
+
+voiceSelect.addEventListener('change', e => speak());
+
